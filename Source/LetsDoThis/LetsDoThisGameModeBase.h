1b2c8e0fa25887bc9be9ef9893a1b00936f753d1
Rustam Second_Fry Gubaydullin <secondfry@gmail.com>
2018-09-05 19:17:57 +0300

    Remove ChoosePlayerStart_Implementation from ALetsDoThisGameModeBase

--- Source/LetsDoThis/LetsDoThisGameModeBase.h
+++ Source/LetsDoThis/LetsDoThisGameModeBase.h
@@ -2,12 +2,8 @@
 
 #pragma once
 
-#include <vector>
-
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
-#include "GameFramework/PlayerStart.h"
-#include "Kismet/GameplayStatics.h"
 #include "Maze/RoomDataStruct.h"
 #include "Maze/Maze.h"
 
@@ -21,30 +17,6 @@ class LETSDOTHIS_API ALetsDoThisGameModeBase : public AGameModeBase {
   GENERATED_BODY()
 
 public:
-  AActor* ChoosePlayerStart_Implementation(AController* Player) override {
-    TArray<AActor*> FoundActors;
-    UGameplayStatics::GetAllActorsOfClass(reinterpret_cast<UObject*>(this->GetWorld()), APlayerStart::StaticClass(), FoundActors);
-
-    for (AActor* actor : FoundActors) {
-      APlayerStart* PlayerStart = static_cast<APlayerStart*>(actor);
-      if (PlayerStart == nullptr) { continue; }
-
-      bool IsDefault = PlayerStart->ActorHasTag("default");
-      if (IsDefault) { return actor; }
-
-      continue;
-
-      bool IsTaken = PlayerStart->ActorHasTag("taken");
-      if (IsTaken) { return actor; }
-
-      PlayerStart->Tags.Add("taken");
-      
-      return actor;
-    }
-
-    return FoundActors[0];
-  }
-
   UFUNCTION(BlueprintCallable, Category = "Map Generation")
   static TArray<FRoomDataStruct> generateMap() {
     return Maze::generateMap();
